e15a56cbb227a1412e56e4c468ac6f51369c99b0
Mark Melnic <markhappy200@gmail.com>
2021-01-13 14:42:25 +0200

    Fixed returning the highest card

--- bots/losingbot/losingbot.py
+++ bots/losingbot/losingbot.py
@@ -27,14 +27,14 @@ class Bot:
 			move_set = [move for move in move_set if move[1] > opponent_card % 5]
 			move_set = self.sort_list(move_set)
 			if len(move_set) > 0:
-				return move_set[0][0]
+				return move_set[-1][0]
 
 		if len(moves_not_trump) > 0:
-			return moves_not_trump[0][0]
+			return moves_not_trump[-1][0]
 		else:
 			move_set = [(move, move[0] % 5) for move in moves]
 			move_set = self.sort_list(move_set)
-			return move_set[0][0]
+			return move_set[-1][0]
 
 	def sort_list(self, sub_list): 
 		return(sorted(sub_list, key = lambda x: x[1])) 
