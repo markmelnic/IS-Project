69e8a39408c69bd98af3468e69816180fca65a9e
Mark Melnic <markhappy200@gmail.com>
2021-01-19 15:06:50 +0200

    Added automatic binomial test

--- binomial_test.py
+++ binomial_test.py
@@ -1,5 +1,28 @@
+import os, csv
 from scipy.stats import binom_test
+from rich import print
+from rich.table import Table
 
-test = binom_test(9, n=863, p=0.5, alternative='greater')
+labels, data = [], []
+with os.scandir() as entries:
+    for entry in entries:
+        if ".csv" in entry.name:
+            with open(entry.name, "r", newline="") as csv_file:
+                labels.append(entry.name)
+                data.append(list(csv.reader(csv_file)))
 
-print(1 - test)
+bot = labels[0].split("_")[-1].split(".")[0].split("-")[0]
+labels = [l.split("_")[-1].split(".")[0].split("-")[1] for l in labels]
+
+ml_wins = [len(d) for d in data]
+ml_lost = [int(d[-1][0]) - win for d, win in zip(data, ml_wins)]
+
+tab = Table(header_style="bold magenta", title=bot.upper()+" bot one-tailed binomial test")
+tab.add_column("Opponent")
+tab.add_column("Probability")
+
+for lb, mw, ml in zip(labels, ml_wins, ml_lost):
+    bres = 1 - binom_test(mw, n=mw+ml, p=0.5, alternative='greater')
+    tab.add_row(lb, str(bres))
+
+print("\n", tab, "\n")
