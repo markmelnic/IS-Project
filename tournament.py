7d283880c02088c441184dd9d0bc80d950a44919
Mark Melnic <markhappy200@gmail.com>
2021-01-23 18:29:14 +0200

    Existing seeds tournament runner added

--- tournament.py
+++ tournament.py
@@ -35,43 +35,73 @@ def run_tournament(options):
                     else:
                         games_count, seeds = 0, []
 
+    # load existing seeds
+    if options.existing:
+        seeds_file = "T_Dataset_{}.csv".format(options.existing)
+        with open(seeds_file, "r", newline="") as seeds_data:
+            seeds = [int(seed.split(",")[1]) for seed in list(seeds_data.readlines())]
+    else:
+        seeds = []
+
     print('Playing {} scored games:'.format(int(totalgames)))
     with open(filename, "a", newline="") as t_data:
         t_writer = csv.writer(t_data)
 
-        for a, b in matches:
-            while not scoredgames == options.repeats:
+        if seeds:
+            for a, b in matches:
+                for seed in seeds:
+                    p = [a, b] if random.choice([True, False]) else [b, a]
 
-                p = [a, b] if random.choice([True, False]) else [b, a]
+                    state = State.generate(id=seed, phase=int(options.phase))
 
-                # Generate a state with a random seed
-                seed = random.randint(1000000, 9999999)
-                while seed in seeds:
+                    winner, score = engine.play(bots[p[0]], bots[p[1]], state, options.max_time*1000, verbose=options.verbose, fast=options.fast)
+
+                    if winner is not None:
+                        winner = p[winner - 1]
+                        wins[winner] += score
+
+                        #if winner == options.indexed - 1 and score > 1:
+                        #    t_writer.writerow([games_count + scoredgames, seed])
+
+                        if score > 0:
+                            scoredgames += 1
+                        playedgames += 1
+                        print('Played {} games, {:.0f} scored out of {:.0f} ([yellow]{:.0f}%[/yellow]): [italic green]{}[/italic green] won, seed [red]{}[/red], [black]{}[/black] \r'
+                        .format(playedgames, scoredgames, len(seeds), scoredgames/float(len(seeds)) * 100, botnames[winner], seed, wins))
+
+        else:
+            for a, b in matches:
+                while not scoredgames == options.repeats:
+                    p = [a, b] if random.choice([True, False]) else [b, a]
+
+                    # Generate a state with a random seed
                     seed = random.randint(1000000, 9999999)
-                seeds.append(seed)
+                    while seed in seeds:
+                        seed = random.randint(1000000, 9999999)
+                    seeds.append(seed)
 
-                state = State.generate(id=seed, phase=int(options.phase))
+                    state = State.generate(id=seed, phase=int(options.phase))
 
-                winner, score = engine.play(bots[p[0]], bots[p[1]], state, options.max_time*1000, verbose=options.verbose, fast=options.fast)
+                    winner, score = engine.play(bots[p[0]], bots[p[1]], state, options.max_time*1000, verbose=options.verbose, fast=options.fast)
 
-                if winner is not None:
-                    winner = p[winner - 1]
-                    wins[winner] += score
+                    if winner is not None:
+                        winner = p[winner - 1]
+                        wins[winner] += score
 
-                    if winner == options.indexed - 1 and score > 1:
-                        t_writer.writerow([games_count + scoredgames, seed])
+                        if winner == options.indexed - 1 and score > 1:
+                            t_writer.writerow([games_count + scoredgames, seed])
 
-                    if score > 0:
-                        scoredgames += 1
-                    playedgames += 1
-                    print('Played {} games, {:.0f} scored out of {:.0f} ([yellow]{:.0f}%[/yellow]): [italic green]{}[/italic green] won, seed [red]{}[/red], [black]{}[/black] \r'
-                    .format(playedgames, scoredgames, totalgames, scoredgames/float(totalgames) * 100, botnames[winner], seed, wins))
+                        if score > 0:
+                            scoredgames += 1
+                        playedgames += 1
+                        print('Played {} games, {:.0f} scored out of {:.0f} ([yellow]{:.0f}%[/yellow]): [italic green]{}[/italic green] won, seed [red]{}[/red], [black]{}[/black] \r'
+                        .format(playedgames, scoredgames, totalgames, scoredgames/float(totalgames) * 100, botnames[winner], seed, wins))
 
-    print('Results:')
-    for i, bot in enumerate(bots):
-        games_2 = int(wins[i] / 100000)
-        games_3 = int(wins[i] % 100000)
-        print(' '*4 + 'bot {}: {} points, won {} [purple]2[/purple] point games, {} [purple]3[/purple] point games, {} total'.format(bot, wins[i], games_2, games_3, games_2 + games_3))
+        print('Results:')
+        for i, bot in enumerate(bots):
+            games_2 = int(wins[i] / 100000)
+            games_3 = int(wins[i] % 100000)
+            print(' '*4 + 'bot {}: {} points, won {} [purple]2[/purple] point games, {} [purple]3[/purple] point games, {} total'.format(bot, wins[i], games_2, games_3, games_2 + games_3))
 
 
 if __name__ == "__main__":
@@ -111,5 +141,10 @@ if __name__ == "__main__":
                         dest="indexed",
                         help="Chose the wins of which player should be tracked (player 1 / 2)",
                         type=int, default=1)
+
+    parser.add_argument("-e", "--existing",
+                        dest="existing",
+                        help="Choose which dataset to load seeds from",
+                        type=str, default=None)
 
     run_tournament(parser.parse_args())
